08c197125a4920f15aea1f2f85dc8ba569a82a8a
Nayana Shetty <nayanashetty@nayanas-mbp.home>
2018-11-04 23:23:48 +0000

    fixed post

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask, jsonify, request, logging
 import boto3
 from flask_cors import CORS
-
+import json
 
 app = Flask(__name__)
 CORS(app, origins=['http://weather-report-app.s3-website-eu-west-1.amazonaws.com'])
@@ -14,23 +14,24 @@ db = dynamodb.Table('weather-app-city-list')
 def weather_report():
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 	if request.method == 'POST':
-		city = request.args.get('city')
-		print(city)
+		data = request.data
+		dataDict = json.loads(data.decode("utf-8").replace("'",'"'))
+		city = dataDict['city']
 
-		# db.put_item(
-		# 	Item={
-		# 		'city': city,
-		# 	}
-		# )
-		# print("Written data into dynamoDB")
-		# r = requests.get(url.format(city)).json()
-		# weather = {
-		# 	'city' : city,
-		# 	'temperature' : r['main']['temp'],
-		# 	'description' : r['weather'][0]['description'],
-		# 	'icon' : r['weather'][0]['icon'],
-		# }
-		return jsonify({'status': 'added'})
+		db.put_item(
+			Item={
+				'city': city,
+			}
+		)
+		print("Written data into dynamoDB")
+		r = requests.get(url.format(city)).json()
+		weather = {
+			'city' : city,
+			'temperature' : r['main']['temp'],
+			'description' : r['weather'][0]['description'],
+			'icon' : r['weather'][0]['icon'],
+		}
+		return jsonify(weather)
 	
 	res = db.scan(
 		Select='ALL_ATTRIBUTES'
@@ -51,29 +52,5 @@ def weather_report():
 
 	#print(weather_data)
 
-	return jsonify({'weather': weather_data})
-
-
-@app.route('/weather/add', methods=['POST'])
-def weather_add_report():
-	city = request.args.get("city", "")
-	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-	#args = request.args.to_dict()
-	#city = request.args.get('city')
-	#print(args)
-
-		# db.put_item(
-		# 	Item={
-		# 		'city': city,
-		# 	}
-		# )
-		# print("Written data into dynamoDB")
-		# r = requests.get(url.format(city)).json()
-		# weather = {
-		# 	'city' : city,
-		# 	'temperature' : r['main']['temp'],
-		# 	'description' : r['weather'][0]['description'],
-		# 	'icon' : r['weather'][0]['icon'],
-		# }
-	return jsonify({'status': city})
+	return jsonify(weather_data)
 
--- frontend/main.js
+++ frontend/main.js
@@ -3,12 +3,16 @@ $(function (){
 	var $cities = $('#cities');
 	var $city = $('#city');
 
+	function addCity(city_data) {
+		$cities.append(`<tr><td><img src="http://openweathermap.org/img/w/${city_data.icon}.png" alt="Image"></td> <td><b>${city_data.city}</b><br/> ${city_data.temperature} F <br />${city_data.description}</td></tr>`);
+	}
+
 	$.ajax({
 		type: 'GET',
 		url: 'https://uisnrbk429.execute-api.eu-west-1.amazonaws.com/dev/weather',
 		success: function (weather_data){
-			$.each(weather_data.weather, function(id, wcity){
-				$cities.append(`<tr><td><img src="http://openweathermap.org/img/w/${wcity.icon}.png" alt="Image"></td> <td><b>${wcity.city}</b><br/> ${wcity.temperature} F <br />${wcity.description}</td></tr>`)
+			$.each(weather_data, function(id, city_data){
+				addCity(city_data);
 			});
 		},
 		error: function (){
@@ -22,11 +26,11 @@ $(function (){
 		};
 		$.ajax({
 			type: 'POST',
-			url: 'https://uisnrbk429.execute-api.eu-west-1.amazonaws.com/dev/weather/add',
-			data: addcity,
+			contentType: "application/json",
+			url: 'https://uisnrbk429.execute-api.eu-west-1.amazonaws.com/dev/weather',
+			data: JSON.stringify(addcity),
 			success: function (city_data){
-				console.log(city_data)
-//				$cities.append(`<tr><td><img src="http://openweathermap.org/img/w/${city_data.icon}.png" alt="Image"></td> <td><b>${city_data.city}</b><br/> ${city_data.temperature} F <br />${city_data.description}</td></tr>`)
+				addCity(city_data);
 			},
 			error: function (){
 				alert('error saving new city');
